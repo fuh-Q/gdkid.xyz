b1ca0e632bf06f26cdf7930606c9742007ec2c0b
GDKID <bawbeefarted@gmail.com>
2022-12-10 13:30:42 -0500

    Remove dynamic route

--- src/pages/api/notes.ts
+++ src/pages/api/notes.ts
@@ -1,9 +1,9 @@
-import type { RawNote } from "../../../types/Note";
+import type { RawNote } from "../../types/Note";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import LockoutLimiter from "../../../utils/limiter";
+import LockoutLimiter from "../../utils/limiter";
 
-import { Pool, QueryResult } from "pg";
+import { Pool } from "pg";
 import { config } from "dotenv";
 
 config();
@@ -15,21 +15,11 @@ const pool = new Pool({
     connectionTimeoutMillis: 5000,
 });
 
-export const limiter = new LockoutLimiter({
+const limiter = new LockoutLimiter({
 	rate: 3,
 	per: 2,
 	keyGen: (r: NextApiRequest) => r.socket.remoteAddress || "",
 })
-
-export async function getNotes(id?: number): Promise<QueryResult<RawNote>> {
-    const condition = id ? "WHERE id = $1" : "ORDER BY id DESC"
-    const query = `SELECT * FROM notes ${condition}`;
-    const conn = await pool.connect();
-    const data = await conn.query<RawNote>(query, id ? [id] : []);
-    conn.release();
-
-    return data;
-}
 
 export default async function handler(
 	req: NextApiRequest,
@@ -38,11 +28,14 @@ export default async function handler(
 	if (limiter.isRateLimited(req)) {
 		return res.status(429).send("too many requests");
 	}
-
 	if (req.method !== "GET") {
 		return res.status(405).send("method not allowed");
 	}
 
-	const data = await getNotes();
+	const query = `SELECT * FROM notes ORDER BY id DESC`;
+	const conn = await pool.connect();
+	const data = await conn.query<RawNote>(query);
+	conn.release();
+
 	return res.status(200).json(data.rows);
 }
