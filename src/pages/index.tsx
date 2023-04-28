5fead9ac10c1e64f8ec52d7ebd5d50dca2bafd17
GDKID <bawbeefarted@gmail.com>
2023-04-28 08:34:36 -0400

    right change the github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
             <div className="site-body">
                 <div className="letters">
-                    <LetterButton full="Github" redirect="https://github.com/kidgd" newTab={true} />
+                    <LetterButton full="Github" redirect="https://github.com/fuh-Q" newTab={true} />
                     <LetterButton full="Discord server" redirect="https://discord.gg/gKEKpyXeEB" newTab={true} />
                     <LetterButton full="Artwork" redirect="/artwork" preceding={true} />
                     <LetterButton full="Idk" redirect="/idk" />
